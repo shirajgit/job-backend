e123f5461006c055a0d84743a66c79849beb73b8
SHIRAJMUJAWAR <shirajmujawar03@gmail.com>
2026-01-14 12:52:54 +0530

    Fix start file

--- server.js
+++ server.js
@@ -107,7 +107,7 @@ app.post("/contact", async (req, res) => {
 });
 
 // ---------- Server ----------
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
